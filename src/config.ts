963b6bd726acd0debf4c429a9215b62cd38a1186
Geratdezir <tsk784@gmail.com>
2026-09-16 22:07:57 -0700

    Shrink lensing footprint toward black hole

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,7 @@ export const CONFIG = {
     diskTurbulence: 0.72,
     haloStrength: 0.14,
     bloomStrength: 0.32,
+    lensScale: 0.84,
     einsteinRadius: 1.34,
     criticalCompression: 0.13,
     lensingExtent: 4.0,
